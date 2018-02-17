5e06795c08116bc2647c87473e921404263763ba
lex-attax <lex.pavlovic@gmail.com>
2018-02-17 11:27:54 +0100

    Add files via upload

---
+++ typescriptBasics/04-generics/generics.ts
@@ -0,0 +1,11 @@
+// generics are types which can hold/use several types
+// we are only touching the very basics here
+
+// consider the array object
+
+let numberArray: Array<number> // this array will only accept numbers
+
+// try to initialize it with strings
+
+// numberArray = ['test'] // -> error
+numberArray = [1,2,3]
