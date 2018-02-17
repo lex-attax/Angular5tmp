999ce0974452156d633dc531fb72351f9e401540
lex-attax <lex.pavlovic@gmail.com>
2018-02-17 11:00:20 +0100

    Add files via upload

---
+++ typescriptBasics/02-classes/classes.ts
@@ -0,0 +1,32 @@
+class Car {
+    engineName: string;
+    gears: number;
+    private speed: number; //private property: only accessible from inside the class
+
+    constructor(speed: number) {
+        this.speed = speed || 0
+    }
+
+    accelerate(): void {
+        this.speed++
+    }
+
+    throttle(): void {
+        this.speed--
+    }
+
+    getSpeed(): void {
+        console.log(this.speed)
+    }
+
+    static numberOfWheels(): number {
+        return 4
+    }
+}
+// Instantiate (create) an object from a class
+
+let car = new Car(5)
+car.accelerate()
+car.getSpeed()
+
+console.log(Car.numberOfWheels())
